64d869a7e9bf0a8023d6125d61b259eaf1c60cd0
ahinski <ahinski.denis@gmail.com>
2021-12-17 00:12:00 +0300

    add selection sort

---
+++ labs/SelectionSort.py
@@ -0,0 +1,11 @@
+def selection_sort(array):
+    for i in range(len(array)-1):
+        min_index = i
+        for j in range(i+1, len(array)-1):
+            if array[j] < array[min_index]:
+                min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
+    return array
+
+test = [6,4,6,8,88,343,32,1]
+print(selection_sort(test))
