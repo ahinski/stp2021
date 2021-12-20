e7423ba4637891be3ccf58d85e98ed10f6823f38
ahinski <ahinski.denis@gmail.com>
2021-12-20 12:15:11 +0300

    add view

--- library/books/views.py
+++ library/books/views.py
@@ -1,3 +1,7 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
